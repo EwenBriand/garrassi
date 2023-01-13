2c7cb20077971e7e35945aa257d57e6a86b14bfd
EwenBriand <ewen.briand@epitech.eu>
2023-01-13 17:59:45 +0100

    [ADD] lot of shit

---
+++ garrasi/dist/cinoche.js
@@ -0,0 +1,45 @@
+// console.log("hello", document)
+// console.log(document.body.style.left)
+// document.body.style.left = document.    body.style.height - 400 + 'px';
+
+
+
+    // let div = document.createElement('div');
+    // div.className = "sidenav"
+
+
+
+
+// let old_body = document.body
+// console.log("body :", old_body)
+// let body = document.createElement('body')
+// let div = document.createElement('div')
+// let col1 = document.createElement('div')
+// let col2 = document.createElement('div')
+// let html = document.createElement('html')
+
+// div.style = "display:block; width:100%;"
+// col1.style = "width:80%; float: left; display: inline-block;"
+// col2.style = "width:20%; float: right; display: inline-block; position: fixed;"
+// col2.innerHTML = "Your left"
+// col2.className = "sidenav"
+// col1.append(document.body);
+
+// div.append(col1);
+// div.append(col2);
+
+// html.append(div);
+// document.delete("head")
+// document.delete("body")
+// document.html.append(div);
+// console.log(html);
+// console.log(document)
+
+
+
+
+// const featureBoxRight = document.querySelector('div.featured-box-right');
+
+// if (featureBoxRight) {
+// featureBoxRight.remove();
+// }
--- garrasi/src/main.js
+++ garrasi/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log("From popup.js")
+    console.log(request.msg)
+})
 createApp(App).mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
