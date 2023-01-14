83f425f2e217f37b2fd5806ffd9c52f5ec36da1a
Thomas LAPRIE <thomas.laprie@epitech.eu>
2023-01-14 15:11:52 +0100

    [UPDATE] delet message if -20 freeze

--- garrasi/src/App.vue
+++ garrasi/src/App.vue
@@ -3,7 +3,7 @@
   <div style="margin-top: 60px; margin-bottom: 70px;">
     <p v-if="allComments[channelName].length == 0" style="margin-left: 135px;" >No Message</p>
     <div v-for="(user,index) in allComments[channelName]" style="margin-top: 20px; margin-bottom: 20px;" :key="user">
-      <div class="container user row">
+      <div class="container user row" v-if="((user.fire.length - 1) - (user.freeze.length - 1)) > -20">
         <div class="col-1" style="margin: auto; margin-right: 20px; margin-left: -10px;">
           <svg xmlns="http://www.w3.org/2000/svg" style="display:inline-block;" :color="setColorToFire(index)" @click="Fire(index, nameUser)" width="25" height="25" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16"><path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z"/></svg>
           <p style="display:inline-block;margin-right:10px;margin-left: 4px;"> {{ (user.fire.length - 1) - (user.freeze.length - 1) }}</p>
